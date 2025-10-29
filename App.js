82f111518c4e993808d0d94fbee6d1c380fdbfd7
Mohammad Sakib <md8628040@gmail.com>
2025-10-29 22:10:33 +0530

    using Raect.js and Tailwind

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Cards from "./Components/Cards";
+import Filter from "./Components/Filter";
+import {apiUrl, filterData} from "./data";
+import { useState, useEffect } from 'react';
+import Spinner from "./Components/Spinner";
+import { toast } from "react-toastify";
+
+const App = () =>{
+    const [courses, setCourses] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [category, setCategory] = useState(filterData[0].title);
+
+     async function fetchData() {
+        setLoading(true);
+        try{
+           let response = await fetch(apiUrl);
+           let output = await response.json();
+           //output->
+           setCourses(output.data);
+        }
+        catch(error){
+         toast.error("Network me koi dikkat hai");
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[])
+
+    return(
+       <div className="min-h-screen flex flex-col bg-bgDark2">
+        <div>
+            <Navbar/>
+        </div>
+        <div className="bg-bgDark2">
+        <div>
+            <Filter 
+            filterData={filterData}
+            category={category}
+            setCategory={setCategory}
+            />
+        </div>
+        <div  className="w-11/12 flex  max-w-[1200px] mx-auto  flex-wrap
+     justify-center items-center min-h-[50vh]">
+            {
+                loading ? (<Spinner/>) : (<Cards courses={courses} category={category}/>)
+            }
+            </div>
+        </div>
+       </div>
+    );
+};
+ 
+export default App;
+
+    
